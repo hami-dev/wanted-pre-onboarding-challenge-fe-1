e19f79e46e9787d4aa063dfa62014e28e8773050
hami-dev <rinrin.xml@gmail.com>
2022-08-03 18:02:26 +0900

    Add: 회원가입, 로그인 페이지 추가

---
+++ src/pages/auth/Signup.jsx
@@ -0,0 +1,88 @@
+import React, {useEffect, useMemo} from 'react';
+import {useNavigate} from 'react-router-dom';
+import {useForm} from 'react-hook-form';
+
+import Container from 'components/common/Container';
+import Input from 'components/common/Input';
+import Button from 'components/common/Button';
+import ButtonWrapper from 'components/common/ButtonWrapper';
+
+const defaultValues = {
+  email: '',
+  password: '',
+};
+
+const Signup = () => {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    reset,
+    formState: {errors, isValid},
+  } = useForm({reValidateMode: 'onChange', mode: 'onChange', defaultValues});
+
+  const onSubmit = (data) => {
+    // api 연결
+  };
+
+  const onClickCancel = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div>
+      <Container title="회원가입">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Input
+            type="text"
+            placeholder="email"
+            {...register('email', {
+              required: {
+                value: true,
+                message: '이메일을 입력해주세요.',
+              },
+              pattern: {
+                value: /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+                message: '올바른 이메일 형식을 입력해주세요.',
+              },
+            })}
+          />
+          <div>{errors?.email?.message}</div>
+          <Input
+            type="password"
+            placeholder="password"
+            {...register('password', {
+              required: {
+                value: true,
+                message: '비밀번호를 입력해주세요.',
+              },
+              minLength: {
+                value: 8,
+                message: '비밀번호는 8글자 보다 길어야 합니다.',
+              },
+            })}
+          />
+          <div>{errors?.password?.message}</div>
+          <ButtonWrapper>
+            <>
+              {useMemo(
+                () => (
+                  <Button type="submit" className="blue large" disabled={!isValid} style={{width: '120px'}}>
+                    가입하기
+                  </Button>
+                ),
+                [isValid],
+              )}
+            </>
+            <Button type="button" className="large" style={{width: '120px'}} onClick={onClickCancel}>
+              취소
+            </Button>
+          </ButtonWrapper>
+        </form>
+      </Container>
+    </div>
+  );
+};
+
+export default Signup;
