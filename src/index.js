0968e787ee5edde03b70eb127dbde14ae31676c7
hami-dev <rinrin.xml@gmail.com>
2022-08-03 17:40:15 +0900

    Design: theme 추가

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import {ThemeProvider} from 'styled-components';
+import defaultTheme from 'styles/theme';
+
 import 'styles/reset.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/styles/theme.js
@@ -0,0 +1,34 @@
+import {css} from 'styled-components';
+
+const defaultTheme = {
+  black: '#00072E',
+  white: '#ffffff',
+  blue: '#0F71FA',
+  lightblue: '#26ACFF',
+  gray: '#737373',
+  lightgray: '#d1d6de',
+  fonts: {
+    title: css`
+      font-size: 30px;
+      line-height: 36pxpx;
+      font-weight: 700;
+    `,
+    subTitle: css`
+      font-size: 24px;
+      line-height: 30px;
+      font-weight: 400;
+    `,
+    body: css`
+      font-size: 15px;
+      line-height: 19px;
+      font-weight: 400;
+    `,
+    description: css`
+      font-size: 12px;
+      line-height: 15px;
+      font-weight: 400;
+    `,
+  },
+};
+
+export default defaultTheme;
