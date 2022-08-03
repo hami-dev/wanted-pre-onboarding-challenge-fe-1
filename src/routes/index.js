f12610fffd4e5a74b9b812f972334183ccd1be4c
hami-dev <rinrin.xml@gmail.com>
2022-08-03 17:40:52 +0900

    Fix: router- 회원가입, 로그인 분리하여 추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,15 @@ import styled from 'styled-components';
 import {Routes, Route} from 'react-router-dom';
 
 import Main from 'pages/Main';
+import Signup from 'pages/auth/Signup';
+import Signin from 'pages/auth/Signin';
 
 const RoutesIndex = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/auth/signup" element={<Signup />} />
+      <Route path="/auth/signin" element={<Signin />} />
     </Routes>
   );
 };
