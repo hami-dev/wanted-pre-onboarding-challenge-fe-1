1e4122241a01f5c81bf537fe8130b49237cfe4b2
hami-dev <rinrin.xml@gmail.com>
2022-08-08 18:49:27 +0900

    Fix: 투두페이지에 로그아웃버튼 추가, 로그아웃 기능 연결

--- src/components/todos/TodoList.jsx
+++ src/components/todos/TodoList.jsx
@@ -7,9 +7,11 @@ import {useQuery} from '@tanstack/react-query';
 import {Todos} from 'api/Todos';
 
 import {todoStore} from 'pages/todo/Todos';
+import {Utils} from 'utils';
 
 import TodoItem from 'components/todos/TodoItem';
 import Button from 'components/common/Button';
+import ButtonWrapper from 'components/common/ButtonWrapper';
 
 const TodoList = () => {
   const navigate = useNavigate();
@@ -30,18 +32,30 @@ const TodoList = () => {
           ))}
         </>
       )}
-
-      <Button
-        type="button"
-        className="large blue"
-        onClick={() => {
-          setEditTodo(false);
-          setCreateTodo(true);
-          navigate('/');
-        }}
-      >
-        새 TODO 등록하기
-      </Button>
+      <ButtonWrapper direction="column">
+        <Button
+          type="button"
+          className="large blue"
+          onClick={() => {
+            setEditTodo(false);
+            setCreateTodo(true);
+            navigate('/');
+          }}
+        >
+          새 TODO 등록하기
+        </Button>
+        <Button
+          type="button"
+          className="textButton grayText"
+          onClick={() => {
+            Utils.removeLocalStorage('token');
+            window.alert('로그아웃되어 로그인 페이지로 이동합니다.');
+            navigate('/auth/signin');
+          }}
+        >
+          유저 로그아웃 하기
+        </Button>
+      </ButtonWrapper>
     </Wrapper>
   );
 };
@@ -49,5 +63,6 @@ const TodoList = () => {
 export default TodoList;
 
 const Wrapper = styled.div`
-  padding-bottom: 50px;
+  border-right: 1px solid ${({theme}) => theme.lightgray};
+  padding-right: 20px;
 `;
