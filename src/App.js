47f44af95f821765e0394e531135f1290e88799f
hami-dev <rinrin.xml@gmail.com>
2022-08-08 18:09:24 +0900

    Fix: 리액트쿼리 사용

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
+import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import RoutesIndex from 'routes';
 
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+
+const rqStore = React.createContext();
+const {Provider} = rqStore;
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <BrowserRouter>
-          <RoutesIndex />
-        </BrowserRouter>
-      </div>
+      <Provider value={{queryClient}}>
+        <div className="App">
+          <BrowserRouter>
+            <RoutesIndex />
+          </BrowserRouter>
+        </div>
+      </Provider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 }
 
 export default App;
+export {rqStore};
